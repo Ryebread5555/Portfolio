08b7f5dd55d61d9de2ca6586d0673a4756d2b956
Ryebread5555 <rypetty55@gmail.com>
2025-06-29 15:06:35 -0500

    updated project sizing

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,19 +7,19 @@ const ProjectCard = ({ name, image, github, description, skills }) => {
 
   return (
     <motion.div
-      className="bg-white shadow-md rounded-lg p-4 w-full transform transition duration-300"
+      className="bg-white shadow-md rounded-xl p-4 w-full h-full flex flex-col justify-between transform transition duration-300"
       initial={{ opacity: 0, y: 30 }}
       whileInView={{ opacity: 1, y: 0 }}
-      whileHover={{ scale: 1.05 }}
+      whileHover={{ scale: 1.03 }}
       viewport={{ once: true }}
       transition={{ duration: 0.5 }}>
-
-      <div className="w-full aspect-video overflow-hidden rounded-md mb-4">
-      <img src={image} alt={name} className="w-full h-48 object-cover" />
+      
+      <div className="w-full h-40 overflow-hidden rounded-md mb-4">
+        <img src={image} alt={name} className="w-full h-full object-cover" />
       </div>
       <h3 className="text-lg font-bold mb-2">{name}</h3>
       <p className="text-sm text-gray-600 mb-2">{description}</p>
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center mt-auto">
         <span className="text-xs text-gray-500">{skills}</span>
         <BsGithub
           className="text-2xl cursor-pointer"
